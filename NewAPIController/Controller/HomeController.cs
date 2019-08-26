ad992603091f5d6379a8a6e3d32c84da4a9786ef
Ehis-Anthony <speaktoehis77@gmail.com>
2019-08-26 13:32:59 +0100

    Initial commit - moving project

---
+++ NewAPIController/Controller/HomeController.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NewAPIController.Models;
+
+namespace NewAPIController.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class HomeController : Controller
+    {
+        private IRepository repository { get; set; }
+        public HomeController(IRepository repo) => repository = repo;
+        public ViewResult Index() => View(repository.Reservations);
+
+
+        [HttpPost]
+        public IActionResult AddReservation(Reservation reservation)
+        {
+            repository.AddReservation(reservation);
+            return RedirectToAction("Index");
+        }
+    }
+}
